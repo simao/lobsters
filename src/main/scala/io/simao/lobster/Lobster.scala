0cf33aadc1ffce24d1201a5ebd5b6e6afbf2f7a1
Simão Mata <me@simao.io>
2014-12-27 11:31:40 +0000

    Simple deploy/run scripts

--- src/main/scala/io/simao/lobster/Lobster.scala
+++ src/main/scala/io/simao/lobster/Lobster.scala
@@ -11,7 +11,7 @@ import scala.concurrent.duration._
 class CliOpts(args: Seq[String]) extends ScallopConf(args) {
   val jdbc = opt[String]("jdbc", descr = "JDBC Connection string", default = Some("jdbc:sqlite:saved_feeds.db"))
   val days = opt[Int]("days", descr = "Only consider stories newer than this number of days", default = Some(2))
-  val score = opt[Int]("score", descr = "Minimum number of votes to tweet a story", default = Some(4))
+  val score = opt[Int]("score", descr = "Minimum number of votes to tweet a story", default = Some(10))
   val tweet = toggle("tweet", descrYes = "Tweet items", default = Some(true), noshort = true)
   val drop = toggle("drop", descrYes = "Drop item database. CAUTION", default = Some(false), noshort = true)
 }
